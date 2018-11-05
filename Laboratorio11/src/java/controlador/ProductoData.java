9e09651b772805983e3e374ea1cd435d6083f5ac
jfabiantorres <john.timoteo@tecsup.edu.pe>
2018-11-04 19:50:46 -0500

    Nuevos cambios para el laboratorio 11

--- Laboratorio11/src/java/controlador/ProductoData.java
+++ Laboratorio11/src/java/controlador/ProductoData.java
@@ -19,7 +19,68 @@ public class ProductoData implements Serializable{
     private static final long serialVersionUID = 1L;
     Conexion c = new Conexion();
     
-    //
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int stock;
+    private int alm_codigo;
+    private int sum_codigo;
+
+    public Conexion getC() {
+        return c;
+    }
+
+    public void setC(Conexion c) {
+        this.c = c;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getAlm_codigo() {
+        return alm_codigo;
+    }
+
+    public void setAlm_codigo(int alm_codigo) {
+        this.alm_codigo = alm_codigo;
+    }
+
+    public int getSum_codigo() {
+        return sum_codigo;
+    }
+
+    public void setSum_codigo(int sum_codigo) {
+        this.sum_codigo = sum_codigo;
+    }
     
     public List<Producto> getProductos () {
         
@@ -50,7 +111,7 @@ public class ProductoData implements Serializable{
         return records;
     }
     
-    /*
+    
     public void registrarProducto() {
         PreparedStatement sen;
         String sql = "INSERT INTO PRODUCTO VALUES (?, ?, ?, ?, ?, ?)";
@@ -58,8 +119,12 @@ public class ProductoData implements Serializable{
             
             sen = c.getConnection().prepareStatement(sql);
             
-            sen.setString(1, empresa);
-            sen.setInt(2, ruc);
+            sen.setString(1, codigo);
+            sen.setString(2, nombre);
+            sen.setDouble(3, precio);
+            sen.setInt(4, stock);
+            sen.setInt(5, alm_codigo);
+            sen.setInt(6, sum_codigo);
             
             sen.executeUpdate();
             
@@ -67,10 +132,10 @@ public class ProductoData implements Serializable{
             e3.printStackTrace();
         }
         
-        empresa = "";
-        ruc = 0;
+        //empresa = "";
+        //ruc = 0;
         
     }
     
-*/
+
 }
